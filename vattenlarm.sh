0afa680ddd0705cd81ab22fb42f95027b54a50d3
Eric Druid <eric.druid@gmail.com>
2021-03-20 09:24:25 +0100

    Allow overrids of files and email

--- vattenlarm.sh
+++ vattenlarm.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 dir=`dirname "$0"`
-locks="$dir/locks"
-file="$dir/vattennivå.csv"
-email=felanmalan@skrytetorp.se
-email=eric.druid@gmail.com
+
+locks=${locks:-"$dir/locks"}
+file=${file:-"$dir/vattennivå.csv"}
+alarm_log=${alarm_log:-"$dir/larm.log"}
+email=${email:-felanmalan@skrytetorp.se}
+
 compareDate=`date '+%F %H:%M' --date='10 hours ago'`
 nowLevel=`tail -n 5 "$file" | awk '{print $4}' | sort -n | head -n 3 | tail -n 1`
 thenLevel=`grep "$compareDate" -C 2 "$file" | awk '{print $4}' | sort -n | head -n 3 | tail -n 1`
@@ -13,7 +15,7 @@ diff=`expr $nowLevel - $thenLevel`
 if [ $diff -lt -40 ]
 then
     msg="Läckagelarm: $diff l på 10 timmar! Från $thenLevel till $nowLevel liter."
-    echo "`date '+%F %H:%M'` $msg" >> "$dir/larm.log"
+    echo "`date '+%F %H:%M'` $msg" >> "$alarm_log"
     if [ `expr $(date +%s) - $(stat -c %Y "$locks/leak.lock") \> 3600` ]
     then
         echo "`date`\n$msg" | mail -s "[Expansionskärl] $msg" "$email"
@@ -23,7 +25,7 @@ fi
 if [ $nowLevel -lt 40 ]
 then
     msg="Kritisk nivå larm: $nowLevel l kvar!"
-    echo "`date '+%F %H:%M'` $msg" >> "$dir/larm.log"
+    echo "`date '+%F %H:%M'` $msg" >> "$alarm_log"
     if [ `expr $(date +%s) - $(stat -c %Y "$locks/level1.lock") \> 3600` ]
     then
         echo "`date`\n$msg" | mail -s "[Expansionskärl] $msg" "$email"
@@ -32,7 +34,7 @@ then
 elif [ $nowLevel -lt 50 ]
 then
     msg="Låg nivå larm: $nowLevel l kvar!"
-    echo "`date '+%F %H:%M'` $msg" >> "$dir/larm.log"
+    echo "`date '+%F %H:%M'` $msg" >> "$alarm_log"
     if [ `expr $(date +%s) - $(stat -c %Y "$locks/level2.lock") \> 3600 \* 24` ]
     then
         echo "`date`\n$msg" | mail -s "[Expansionskärl] $msg" "$email"
