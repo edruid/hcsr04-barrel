afec183f9ec1517d18911958af2743f4cca82799
Eric Druid <eric.druid@gmail.com>
2021-03-23 20:00:09 +0100

    Change alarm levels and timings

--- vattenlarm.sh
+++ vattenlarm.sh
@@ -7,14 +7,14 @@ file=${file:-"$dir/vattennivå.csv"}
 alarm_log=${alarm_log:-"$dir/larm.log"}
 email=${email:-felanmalan@skrytetorp.se}
 
-compareDate=`date '+%F %H:%M' --date='10 hours ago'`
+compareDate=`date '+%F %H:%M' --date='24 hours ago'`
 nowLevel=`tail -n 5 "$file" | awk '{print $4}' | sort -n | head -n 3 | tail -n 1`
 thenLevel=`grep "$compareDate" -C 2 "$file" | awk '{print $4}' | sort -n | head -n 3 | tail -n 1`
 diff=`expr $nowLevel - $thenLevel`
 
-if [ $diff -lt -40 ]
+if [ $diff -lt -50 ]
 then
-    msg="Läckagelarm: $diff l på 10 timmar! Från $thenLevel till $nowLevel liter."
+    msg="Läckagelarm: $diff l på 24 timmar! Från $thenLevel till $nowLevel liter."
     echo "`date '+%F %H:%M'` $msg" >> "$alarm_log"
     if [ `expr $(date +%s) - $(stat -c %Y "$locks/leak.lock")` -gt 3600 ]
     then
@@ -31,7 +31,7 @@ then
         echo "`date`    $msg" | mail -s "[Expansionskärl] $msg" "$email"
         touch "$locks/level1.lock"
     fi
-elif [ $nowLevel -lt 50 ]
+elif [ $nowLevel -lt 60 ]
 then
     msg="Låg nivå larm: $nowLevel l kvar!"
     echo "`date '+%F %H:%M'` $msg" >> "$alarm_log"
